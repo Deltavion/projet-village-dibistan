4a46e8fe6e741b656457efffce69e8dc11096be6
Deltavion <89981881+Deltavion@users.noreply.github.com>
2022-03-06 09:10:53 +0100

    home page content

--- asso-website/src/components/Header.js
+++ asso-website/src/components/Header.js
@@ -5,6 +5,7 @@ import classes from '../styles/Header.module.scss'
 function Header() {
     return (
         <div className={classes.header}>
+            <h1>Dibistassociation</h1>
             <Link to="/" className={classes.header__links}>Accueil</Link>
             <Link to="/donations" className={classes.header__links}>Donations</Link>
             <Link to="/objectifs" className={classes.header__links}>Objectifs</Link>
--- asso-website/src/pages/Home.js
+++ asso-website/src/pages/Home.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import classes from '../styles/Home.module.scss';
 import Header from '../components/Header';
+import logo from '../assets/gradient_bilinear.jpg';
 
 function Home() {
     return (
         <div className={classes.home}>
             <Header />
             <h1 className={classes.home__title}>Home</h1>
+<<<<<<< Updated upstream
             <h2>Présentation</h2>
+=======
+            <div className={classes.home__timeline}>
+                <div className={classes.home__timeline__point}>
+                    <img src={logo} alt="img" />
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
+                    labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi
+                    animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
+                    aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia
+                    pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
+                    commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa
+                    proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
+                    eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+                    Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et
+                    culpa duis.</p>
+                </div>
+                <div className={classes.home__timeline__point}>
+                    <img src={logo} alt="img" />
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
+                    labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi
+                    animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
+                    aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia
+                    pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
+                    commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa
+                    proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
+                    eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+                    Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et
+                    culpa duis.</p>
+                </div>
+                <div className={classes.home__timeline__point}>
+                    <img src={logo} alt="img" />
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim
+                    labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi
+                    animcupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est
+                    aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia
+                    pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
+                    commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa
+                    proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
+                    eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
+                    Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et
+                    culpa duis.</p>
+                </div>
+            </div>
+>>>>>>> Stashed changes
         </div>
     );
 }
